e436f39042897416db6bbeaaa57642f54890a87b
ivdekov <ivdekov@gmail.com>
2017-08-14 14:48:50 -0500

    feat: Create ListCategories component, connect it to react-redux

---
+++ src/Components/ListCategories.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchCategories } from '../actions'
+
+class ListCategories extends Component {
+  componentDidMount() {
+    this.props.fetchCategories()
+  }
+  render() {
+    return (
+      <div>
+        <div className="page-header">
+          <h1>Categories</h1>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <ul className="list-group">
+              {this.props.categories.map(category =>
+                <li className="list-group-item" key={category.path}>
+                  <a href="#">
+                    {category.name}
+                  </a>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    categories: state.categories
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchCategories: () => dispatch(fetchCategories())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListCategories)

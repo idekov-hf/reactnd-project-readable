0e8f57a7b732078e1c5f4d9489a2bb97d6d5e0e2
ivdekov <ivdekov@gmail.com>
2017-08-15 15:21:02 -0500

    feat: Add actions for fetching and receiving all posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,3 +10,12 @@ export const fetchCategories = () => dispatch =>
   APIUtil.fetchCategories().then(({ categories }) =>
     dispatch(receiveCategories(categories))
   )
+
+export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+export const receivePosts = posts => ({
+  type: RECEIVE_POSTS,
+  posts
+})
+
+export const fetchPosts = () => dispatch =>
+  APIUtil.fetchPosts().then(posts => dispatch(receivePosts(posts)))

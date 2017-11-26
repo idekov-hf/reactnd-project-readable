13d4b2ea2b75e9210ecb7e9037cb652b42db863d
ivdekov <ivdekov@gmail.com>
2017-11-26 16:06:37 -0600

    Feat: install and style react-modal

---
+++ src/styles/modal-styles.js
@@ -0,0 +1,11 @@
+export const commentModalStyles = {
+  overlay: {
+    backgroundColor: 'rgba(255, 255, 255, 0.9)'
+  },
+  content: {
+    top: '35%',
+    left: '40px',
+    right: '40px',
+    bottom: 'auto'
+  }
+}
